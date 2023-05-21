23c32de3488ea61c9bc53db6e2eedee87d2459d2
Dferna40 <dferna40@gmail.com>
2023-05-21 22:33:44 +0200

    Nuevo login

--- src/main/java/org/davidfernandez/rankingseries/security/WebSecurityConfig.java
+++ src/main/java/org/davidfernandez/rankingseries/security/WebSecurityConfig.java
@@ -58,7 +58,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
      */
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/registro**","/serie**","/valoracion/**", "/js/**", "/css/**", "/img/**", "/assets/**", "/layout/**")
+		http.authorizeRequests().antMatchers("/registro**","/serie**","/valoracion/**", "/js/**", "/css/**","/css/*.scss", "/img/**", "/assets/**", "/layout/**")
 				.permitAll().anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll().and()
 				.logout().invalidateHttpSession(true).clearAuthentication(true)
 				.logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login?logout")
