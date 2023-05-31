5a259b1b3d98ffca4a291d68aa88a1386e89955c
David Fernández Ramírez <david.fernandez@soltel.es>
2023-05-31 17:35:02 +0200

    Segundo commit

--- src/main/java/org/davidfernandez/rankingseries/controller/RegistroLoginController.java
+++ src/main/java/org/davidfernandez/rankingseries/controller/RegistroLoginController.java
@@ -2,10 +2,17 @@ package org.davidfernandez.rankingseries.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.davidfernandez.rankingseries.dto.SerieDTO;
+import org.davidfernandez.rankingseries.dto.UsuarioRegistroDTO;
+import org.davidfernandez.rankingseries.service.UsuarioService;
 import org.davidfernandez.rankingseries.service.ValoracionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -13,10 +20,24 @@ import org.springframework.web.servlet.ModelAndView;
  * Controlador REST que maneja las operaciones relacionadas con el registro del usuario.
  */
 @RestController
-public class RegistroController {
+public class RegistroLoginController {
 	
 	@Autowired
 	private ValoracionService valoracionService;
+	
+	@Autowired
+	private UsuarioService usuarioService;
+	
+	
+	/**
+   * Retorna un objeto ModelAndView que muestra el formulario de registro.
+   *
+   * @return un objeto ModelAndView que muestra el formulario de registro
+   */
+	@ModelAttribute("usuario")
+	public UsuarioRegistroDTO retornarNuevoUsuarioRegistroDTO() {
+		return new UsuarioRegistroDTO();
+	}
 
 	/**
      * Retorna un objeto ModelAndView que carga la pagina de inicio de sesion.
@@ -44,6 +65,29 @@ public class RegistroController {
 		//Vista
 		modelAndView.setViewName("index");
 		return modelAndView;
+	}
+	
+	/**
+     * Registra un usuario en la base de datos.
+     *
+     * @param usuarioRegistroDTO el DTO del usuario a registrar
+     * @param result los resultados de la validacion del formulario
+     * @return un objeto ModelAndView que redirige a la pagina correspondiente
+     */
+	@PostMapping("/registro")
+	public ModelAndView registrarUsuario(@Valid @ModelAttribute("usuario") UsuarioRegistroDTO usuarioRegistroDTO,
+			BindingResult result) {
+
+		ModelAndView modelAndView = new ModelAndView();
+		if (usuarioService.usuarioNoExiste(usuarioRegistroDTO)) {
+			usuarioService.guardarUsuario(usuarioRegistroDTO);
+			modelAndView.setViewName("redirect:/login?exito");
+		} else {
+			modelAndView.setViewName("redirect:/login?NoExito");
+		}
+
+		return modelAndView;
+
 	}
 }
 
--- src/main/java/org/davidfernandez/rankingseries/controller/RegistroUsuarioController.java
+++ src/main/java/org/davidfernandez/rankingseries/controller/RegistroUsuarioController.java
@@ -1,71 +1,71 @@
-package org.davidfernandez.rankingseries.controller;
-
-import javax.validation.Valid;
-
-import org.davidfernandez.rankingseries.dto.UsuarioRegistroDTO;
-import org.davidfernandez.rankingseries.service.UsuarioService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
-
-/**
- * Controlador REST que maneja las operaciones relacionadas con el registro de usuarios.
- */
-@RestController
-@RequestMapping("/registro")
-public class RegistroUsuarioController {
-
-	@Autowired
-	private UsuarioService usuarioService;
-
-	/**
-     * Retorna un objeto ModelAndView que muestra el formulario de registro.
-     *
-     * @return un objeto ModelAndView que muestra el formulario de registro
-     */
-	@ModelAttribute("usuario")
-	public UsuarioRegistroDTO retornarNuevoUsuarioRegistroDTO() {
-		return new UsuarioRegistroDTO();
-	}
-
-	/**
-     * Retorna un objeto ModelAndView que muestra el formulario de registro.
-     *
-     * @return un objeto ModelAndView que muestra el formulario de registro
-     */
-	@GetMapping
-	public ModelAndView mostrarFormularioRegistro() {
-		ModelAndView modelAndView = new ModelAndView();
-		modelAndView.setViewName("registro");
-		return modelAndView;
-	}
-
-	/**
-     * Registra un usuario en la base de datos.
-     *
-     * @param usuarioRegistroDTO el DTO del usuario a registrar
-     * @param result los resultados de la validacion del formulario
-     * @return un objeto ModelAndView que redirige a la pagina correspondiente
-     */
-	@PostMapping
-	public ModelAndView registrarUsuario(@Valid @ModelAttribute("usuario") UsuarioRegistroDTO usuarioRegistroDTO,
-			BindingResult result) {
-
-		ModelAndView modelAndView = new ModelAndView();
-		if (usuarioService.usuarioNoExiste(usuarioRegistroDTO)) {
-			usuarioService.guardarUsuario(usuarioRegistroDTO);
-			modelAndView.setViewName("redirect:/registro?exito");
-		} else {
-			modelAndView.setViewName("redirect:/registro?NoExito");
-		}
-
-		return modelAndView;
-
-	}
-
-}
+//package org.davidfernandez.rankingseries.controller;
+//
+//import javax.validation.Valid;
+//
+//import org.davidfernandez.rankingseries.dto.UsuarioRegistroDTO;
+//import org.davidfernandez.rankingseries.service.UsuarioService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.validation.BindingResult;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.ModelAttribute;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.servlet.ModelAndView;
+//
+///**
+// * Controlador REST que maneja las operaciones relacionadas con el registro de usuarios.
+// */
+//@RestController
+//@RequestMapping("/registro")
+//public class RegistroUsuarioController {
+//
+//	@Autowired
+//	private UsuarioService usuarioService;
+//
+//	/**
+//     * Retorna un objeto ModelAndView que muestra el formulario de registro.
+//     *
+//     * @return un objeto ModelAndView que muestra el formulario de registro
+//     */
+//	@ModelAttribute("usuario")
+//	public UsuarioRegistroDTO retornarNuevoUsuarioRegistroDTO() {
+//		return new UsuarioRegistroDTO();
+//	}
+//
+//	/**
+//     * Retorna un objeto ModelAndView que muestra el formulario de registro.
+//     *
+//     * @return un objeto ModelAndView que muestra el formulario de registro
+//     */
+//	@GetMapping
+//	public ModelAndView mostrarFormularioRegistro() {
+//		ModelAndView modelAndView = new ModelAndView();
+//		modelAndView.setViewName("login");
+//		return modelAndView;
+//	}
+//
+//	/**
+//     * Registra un usuario en la base de datos.
+//     *
+//     * @param usuarioRegistroDTO el DTO del usuario a registrar
+//     * @param result los resultados de la validacion del formulario
+//     * @return un objeto ModelAndView que redirige a la pagina correspondiente
+//     */
+//	@PostMapping
+//	public ModelAndView registrarUsuario(@Valid @ModelAttribute("usuario") UsuarioRegistroDTO usuarioRegistroDTO,
+//			BindingResult result) {
+//
+//		ModelAndView modelAndView = new ModelAndView();
+//		if (usuarioService.usuarioNoExiste(usuarioRegistroDTO)) {
+//			usuarioService.guardarUsuario(usuarioRegistroDTO);
+//			modelAndView.setViewName("redirect:/registro?exito");
+//		} else {
+//			modelAndView.setViewName("redirect:/registro?NoExito");
+//		}
+//
+//		return modelAndView;
+//
+//	}
+//
+//}
